1bbe3872c2e3d8dcd3d5c80e4eedcdc8bab700d7
Mas73rs <44162345+Mas73rs@users.noreply.github.com>
2024-03-01 07:01:31 +0000

    Add Learning Card functionality

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from tkinter import *
+import pandas as pd
+import random as rd
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+# Load the CSV data
+data = pd.read_csv('./data/french_words.csv')
+to_learn = data.to_dict(orient='records')
+
+# -------------------- NEXT CARD -------------------
+def next_card():
+    language = data.columns[0]  # French
+    # Pick a random word from the selected language: French
+    current_card = rd.choice(to_learn)
+    word = current_card[language]
+    # Update card widget
+    canvas.itemconfig(card_language, text=language)
+    canvas.itemconfig(card_word, text=word)
 
 # -------------------- UI SETUP --------------------
 
@@ -10,8 +26,8 @@ window.title('Flashy')
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 # Load images
-right_img = PhotoImage(file='./images/right.png')
-wrong_img = PhotoImage(file='./images/wrong.png')
+check_img = PhotoImage(file='./images/right.png')
+cross_img = PhotoImage(file='./images/wrong.png')
 card_front_img = PhotoImage(file='./images/card_front.png')
 card_back_img = PhotoImage(file='./images/card_back.png')
 
@@ -22,16 +38,17 @@ canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR
 # Add image to canvas
 canvas.create_image(400, 263, image=card_front_img)
 # Add text to canvas
-canvas.create_text(400, 150, text='Language', fill='black', font=('Ariel', 40, 'italic'))
-canvas.create_text(400, 263, text='word', fill='black', font=('Ariel', 60, 'bold'))
+card_language = canvas.create_text(400, 150, text='', fill='black', font=('Ariel', 40, 'italic'))
+card_word = canvas.create_text(400, 263, text='', fill='black', font=('Ariel', 60, 'bold'))
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Add buttons
-wrong_button = Button(image=wrong_img, highlightbackground=BACKGROUND_COLOR)
-wrong_button.grid(row=1, column=0) # Red button
-right_button = Button(image=right_img, highlightbackground=BACKGROUND_COLOR)
-right_button.grid(row=1, column=1) # Green button
+unknown_button = Button(image=cross_img, highlightbackground=BACKGROUND_COLOR, command=next_card)
+unknown_button.grid(row=1, column=0)  # Red button
+known_button = Button(image=check_img, highlightbackground=BACKGROUND_COLOR, command=next_card)
+known_button.grid(row=1, column=1)  # Green button
 
-
+# Pick a random card
+next_card()
 
 window.mainloop()
