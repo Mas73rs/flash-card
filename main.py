3dbe15317ee5c18fa6fbea87b0adb05f10df6f2c
Mas73rs <44162345+Mas73rs@users.noreply.github.com>
2024-03-01 20:55:17 +0000

    Add Card Flip functionality

--- main.py
+++ main.py
@@ -7,16 +7,34 @@ BACKGROUND_COLOR = "#B1DDC6"
 # Load the CSV data
 data = pd.read_csv('./data/french_words.csv')
 to_learn = data.to_dict(orient='records')
+current_card = {}
+
 
 # -------------------- NEXT CARD -------------------
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     language = data.columns[0]  # French
     # Pick a random word from the selected language: French
     current_card = rd.choice(to_learn)
     word = current_card[language]
     # Update card widget
-    canvas.itemconfig(card_language, text=language)
-    canvas.itemconfig(card_word, text=word)
+    canvas.itemconfig(card_language, text=language, fill='black')
+    canvas.itemconfig(card_word, text=word, fill='black')
+    canvas.itemconfig(card_background, image=card_front_img)
+    # Flip card
+    flip_timer = window.after(3000, func=flip_card)
+
+# -------------------- FLIP CARD -------------------
+def flip_card():
+    language = data.columns[1]  # English
+    word = current_card[language]
+
+    # Update card widget
+    canvas.itemconfig(card_language, text=language, fill='white')
+    canvas.itemconfig(card_word, text=word, fill='white')
+    # Change color
+    canvas.itemconfig(card_background, image=card_back_img)
 
 # -------------------- UI SETUP --------------------
 
@@ -24,6 +42,8 @@ def next_card():
 window = Tk()
 window.title('Flashy')
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
 
 # Load images
 check_img = PhotoImage(file='./images/right.png')
@@ -36,7 +56,7 @@ card_back_img = PhotoImage(file='./images/card_back.png')
 # Create a card widget
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 # Add image to canvas
-canvas.create_image(400, 263, image=card_front_img)
+card_background = canvas.create_image(400, 263, image=card_front_img)
 # Add text to canvas
 card_language = canvas.create_text(400, 150, text='', fill='black', font=('Ariel', 40, 'italic'))
 card_word = canvas.create_text(400, 263, text='', fill='black', font=('Ariel', 60, 'bold'))
@@ -50,5 +70,6 @@ known_button.grid(row=1, column=1)  # Green button
 
 # Pick a random card
 next_card()
+
 
 window.mainloop()
